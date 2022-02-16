e40ed7040f6b1306954834289b21f6835280a2ad
zStevenn <steventje678@hotmail.com>
2022-02-16 14:17:03 +0100

    Added HTML basic to sidemenu.php

--- basic-components/sidemenu.php
+++ basic-components/sidemenu.php
@@ -1,2 +1,10 @@
-<?php
-echo "Side Menu";
+<div class="side-menu">
+  <ul>
+    <li></li>
+    <li><a href="#">a</a></li>
+    <li><a href="#">b</a></li>
+    <li><a href="#">c</a></li>
+    <li><a href="#">d</a></li>
+    <li><a href="#">e</a></li>
+  </ul>
+</div>
