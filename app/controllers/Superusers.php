f31ac6066caca38d5b4c479353696611f583e07f
zStevenn <327068@student.mboutrecht.nl>
2022-03-29 13:56:30 +0200

    Created order page including functionalities - Added if/else statements in controllers/order to check if form has been sent and filled in - placeOrder() function created in models/Superuser.php   This function sanitizes the form inputs, creates a valid SQL query, prepares and executes the query.   When query is executed, string will be returned with a message. - Initiate placeOrder() function in Superuser model. - Added an echo in view - Added CSS to order message - Added JS to orderMessage, hide message after clicking on it

--- app/controllers/Superusers.php
+++ app/controllers/Superusers.php
@@ -14,6 +14,26 @@ class Superusers extends Controller
   public function order()
   {
     $this->view('superusers/order');
+    // Declare variable $orderMessage
+    $orderMessage = null;
+
+    // Check if $_POST has been set
+    if (isset($_POST)) {
+      // Check if submitOrder button has been pressed
+      if (isset($_POST["submitOrder"])) {
+        // Check if order values are filled in
+        if (!empty($_POST["productName"]) && !empty($_POST["productLink"]) && !empty($_POST["productDescription"])) {
+          // Initialize Order() process in model Superuser.php with form details as arguments. 
+          //Returns an array with message CSS and text
+          $m = $this->superUserModel->placeOrder($_POST["productName"], $_POST["productLink"], $_POST["productDescription"]);
+          // Puts the array information into a HTML template. This will be given to the view.
+          $orderMessage = "<div class='". $m["css"] ."' role='alert'>". $m["message"] ." <span>Click to dismiss</span> </div>";
+          // Unset the post values (form values) after saving order in database
+          unset($_POST);
+        }
+      }
+    }
+    $this->view('superusers/order', $orderMessage);
   }
 
   public function approve()
