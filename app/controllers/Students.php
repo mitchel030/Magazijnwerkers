1f7acb39964f3b74035526912bba31c907ac1717
DamienF <329877@student.mboutrecht.nl>
2022-03-29 15:02:54 +0200

    Type: Addition Change: added a script to add variables to the database. Reason: is needed for the userstory

--- app/controllers/Students.php
+++ app/controllers/Students.php
@@ -11,5 +11,41 @@ class Students extends Controller
     $this->view('students/index');
   }
 
+  public function request()
+  {
+    //Default value
+    $data = null;
+    // Check if POST is send
+    if (isset($_POST)) 
+      {
+        // Check if submit button is pressed
+        if (isset($_POST["submit"])) 
+        {
+          // Put post values into variables
+          $product = $_POST["inputProduct"];
+          $amount = $_POST["inputAmount"];
+          $reason = $_POST["inputReason"];
 
+          //Check if variables are not empty
+          if (!empty($product) && !empty($amount) && !empty($reason)) 
+          {
+            //Initiate requestArticle function
+            $message = $this->userModel->requestArticle($product, $amount, $reason);
+
+            if ($message = "success") {
+              //Gelukt
+              $data = "<h1>".$message."</h1>";
+            } else {
+              //Gefaald
+              $data = "<h1>".$message."</h1>";
+            }
+              
+          }
+        }
+        
+
+}
+
+    $this->view('students/request', $data);
+  }
 }
--- app/models/Student.php
+++ app/models/Student.php
@@ -7,4 +7,40 @@ class Student
   {
     $this->db = new Database;
   }
+
+  // Request article function
+  public function requestArticle($product, $amount, $reason) {
+    $p = $product;
+    $a = $amount;
+    $r = $reason;
+
+    //SQL Statement
+    $sql = "INSERT INTO `request` 
+                            (`id`,
+                             `product`, 
+                             `amount`, 
+                             `description`) 
+                            VALUES 
+                            (NULL, 
+                            '$p', 
+                            '$a', 
+                            '$r');";
+
+
+    // Prepare sql statement
+    $this->db->query($sql);
+
+    // Execute sql statement
+    $this->db->execute();
+
+    // Check if query is executed (1 row made)
+    if ($this->db->rowCount() === 1)
+    {
+        // This happens if query is executed successfully
+        return "Success";
+    } else {
+        // This happens if query fails
+        return "Failed";
+    }
+  }
 }
