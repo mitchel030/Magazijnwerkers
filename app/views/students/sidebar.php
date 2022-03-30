56a7ecccc5f2a52f648f033d5bd04ee0c2914639
zStevenn <327068@student.mboutrecht.nl>
2022-03-30 14:21:20 +0200

    Added student redirect on pages/sidemenu.php

--- app/views/students/sidebar.php
+++ app/views/students/sidebar.php
@@ -1,7 +1,7 @@
 <div class="side-menu">
   <ul>
     <img src="<?= URLROOT ?>/public/img/mbo_utrecht_logo_cut.jpg" alt="MBO Utrecht Logo">
-    <li><a href="<?= URLROOT ?>/students/request">Reqeust</a></li>
+    <li><a href="<?= URLROOT ?>/students/request">Request</a></li>
     <li><a href="<?= URLROOT ?>/pages/index">Back to Menu</a></li>
     <span class="copyright">Copyright &copy; 2022</span>
   </ul>
