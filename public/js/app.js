6f7d66e1f231bd6e0233fc9dc2e3f5263556495c
zStevenn <327068@student.mboutrecht.nl>
2022-03-30 12:05:51 +0200

    Removed double view request in controller, added JS to hide orderMessage

--- public/js/app.js
+++ public/js/app.js
@@ -1,36 +1,43 @@
-const openModalButtons = document.querySelectorAll('[data-modal-target]')
-const closeModalButtons = document.querySelectorAll('[data-close-button]')
-const overlay = document.getElementById('overlay')
-
-openModalButtons.forEach(button => {
-  button.addEventListener('click', () => {
-    const modal = document.querySelector(button.dataset.modalTarget)
-    openModal(modal)
-  })
+// Steven JS
+// Hide order message after clicking on it
+const orderMessage = document.querySelector('.superuser--order')
+orderMessage.addEventListener('click', function() {
+  orderMessage.classList.add('hidden')
 })
 
-overlay.addEventListener('click', () => {
-  const modals = document.querySelectorAll('.modal.active')
-  modals.forEach(modal => {
-    closeModal(modal)
-  })
-})
+// const openModalButtons = document.querySelectorAll('[data-modal-target]')
+// const closeModalButtons = document.querySelectorAll('[data-close-button]')
+// const overlay = document.getElementById('overlay')
 
-closeModalButtons.forEach(button => {
-  button.addEventListener('click', () => {
-    const modal = button.closest('.modal')
-    closeModal(modal)
-  })
-})
+// openModalButtons.forEach(button => {
+//   button.addEventListener('click', () => {
+//     const modal = document.querySelector(button.dataset.modalTarget)
+//     openModal(modal)
+//   })
+// })
 
-function openModal(modal) {
-  if (modal == null) return
-  modal.classList.add('active')
-  overlay.classList.add('active')
-}
+// overlay.addEventListener('click', () => {
+//   const modals = document.querySelectorAll('.modal.active')
+//   modals.forEach(modal => {
+//     closeModal(modal)
+//   })
+// })
 
-function closeModal(modal) {
-  if (modal == null) return
-  modal.classList.remove('active')
-  overlay.classList.remove('active')
-}
+// closeModalButtons.forEach(button => {
+//   button.addEventListener('click', () => {
+//     const modal = button.closest('.modal')
+//     closeModal(modal)
+//   })
+// })
+
+// function openModal(modal) {
+//   if (modal == null) return
+//   modal.classList.add('active')
+//   overlay.classList.add('active')
+// }
+
+// function closeModal(modal) {
+//   if (modal == null) return
+//   modal.classList.remove('active')
+//   overlay.classList.remove('active')
+// }
