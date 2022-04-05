60859cbc7f17cdef46a4dd9b8dc186628858157a
zStevenn <327068@student.mboutrecht.nl>
2022-04-05 13:02:15 +0200

    Created working select query for function validateLogin

--- app/models/Login.php
+++ app/models/Login.php
@@ -15,17 +15,19 @@ class Login
     $e = trim(filter_var($email, FILTER_SANITIZE_EMAIL));
     $p = trim(filter_var($password, FILTER_SANITIZE_STRING));
 
-    // Create SQL statement to select email and password from users table where e-mail is the same
-    $sql = 'SELECT email, password FROM tabelUsers where email = "$e"';
+    // Create SQL statement to select email, password and salt from users table where e-mail is user email
+    $sql = 'SELECT `Email`, `Password`, `Salt`, `IsActive` FROM `login` WHERE `Email` = "' . $e . '";';
     // Prepare and execute sql query
     $this->db->query($sql);
-    $this->db->execute();
+    // Put executed information in $data
+    $data = $this->db->resultSet();
 
     // If sql query returns 1 row (E-mail exists in database)
     if ($this->db->rowCount() === 1) {
+      var_dump($data);
       // Execute next code if e-mail exists
-
     } else {
+      var_dump($data);
       // Execute code when e-mail does not exist in database
 
     }
