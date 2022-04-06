ede6ecb770a21cfdb271236fa50207c2013c1641
mitchel030 <317939@student.mboutrecht.nl>
2022-04-06 15:43:12 +0200

    Some code to get the data form users

--- app/models/Warehouseadmin.php
+++ app/models/Warehouseadmin.php
@@ -6,11 +6,19 @@ class Warehouseadmin
   public function __construct()
   {
     $this->db = new Database;
-
-
-
-    
   }
+
+  // Function to retrieve all users
+  public function getAllUsers()
+  {
+    $this->db->query("SELECT * FROM users");
+
+    $result = $this->db->resultSet();
+
+    return $result;
+  }
+
+  // Selects all data from table 'assortment' and returns the resultSet
 
   public function getAssortment()
   {
