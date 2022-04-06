336a4d5f6031c16888ac5d8071c731f53b2e4b3e
mitchel030 <317939@student.mboutrecht.nl>
2022-04-06 15:37:19 +0200

    Small change in sidebar old name viewwarehouse new name assortment

--- app/views/warehouseadmins/sidebar.php
+++ app/views/warehouseadmins/sidebar.php
@@ -5,7 +5,7 @@
     <li><a href="<?= URLROOT ?>/warehouseadmins/lendingitems">Lendingitems</a></li>
     <li><a href="<?= URLROOT ?>/warehouseadmins/managearticles">Managearticles</a></li>
     <li><a href="<?= URLROOT ?>/warehouseadmins/takingitems">Takingitems</a></li>
-    <li><a href="<?= URLROOT ?>/warehouseadmins/viewwarehouse">View Assortment</a></li>
+    <li><a href="<?= URLROOT ?>/warehouseadmins/assortment">View Assortment</a></li>
     <li><a href="<?= URLROOT ?>/warehouseadmins/profile">Profile</a></li>
     <li><a href="<?= URLROOT ?>/pages/index">Back to Menu</a></li>
     <span class="copyright">Copyright &copy; 2022</span>
