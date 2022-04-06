c14204b4f1f6d1d88989833ba90c6da2d96ca63a
mitchel030 <317939@student.mboutrecht.nl>
2022-04-06 10:18:55 +0200

    Made model page for overview-page-warehouseadmin

--- app/models/Warehouseadmin.php
+++ app/models/Warehouseadmin.php
@@ -6,5 +6,20 @@ class Warehouseadmin
   public function __construct()
   {
     $this->db = new Database;
+
+
+
+    
+  }
+
+  public function getAssortment()
+  {
+    $this->db->query("SELECT * FROM assortment");
+
+    $result = $this->db->resultSet();
+
+    return $result;
   }
 }
+
+
