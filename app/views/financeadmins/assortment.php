a6719caa342f0b96d840965062b59ce4077c4180
325972 <325972@student.mboutrecht.nl>
2022-04-13 11:52:53 +0200

    Assortment page database added

--- app/views/financeadmins/assortment.php
+++ app/views/financeadmins/assortment.php
@@ -8,7 +8,21 @@
     </div>
     <!-- Page Content -->
     <div class="content col-10">
-      <h1>Assortment Content</h1>
+      <h1 class="text-center">Assortment</h1>
+      <table class="table table-hover table-striped">
+        <thead>
+          <tr>
+            <th scope="col">Product ID</th>
+            <th scope="col">Name</th>
+            <th scope="col">Total Articles</th>
+            <th scope="col">Individual Price Articles</th>
+            <th scope="col">Total Price Articles</th>
+          </tr>
+        </thead>
+        <tbody>
+        <?php echo $data; ?>
+        </tbody>
+      </table>
     </div>
   </div>
 </div>
