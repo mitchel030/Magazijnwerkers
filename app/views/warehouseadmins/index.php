e74476b6c2329f72dc972a7d03ebd39e0d4cb941
mitchel030 <317939@student.mboutrecht.nl>
2022-05-25 10:03:38 +0200

    Made a create for warehouse crud system

--- app/views/warehouseadmins/index.php
+++ app/views/warehouseadmins/index.php
@@ -10,8 +10,8 @@
       <div class="col-10">
          <div class="jumbotron mt-4">
             <h1 class="display-4">Assortment</h1>
-            <a href="<?= URLROOT ?>/warehouseadmins/create" class="btn btn-sm btn-primary">
-            Create
+            
+            <button id="editBtn" class="modalBtn">Create</button>
             </a>
             <div class="table-responsive mt-5">
                <table class="table">
@@ -35,5 +35,39 @@
       </div>
    </div>
 </div>
+
+<div id="editModal" class="editModal hidden ">
+
+                <!-- Modal content -->
+                <div class="modal-content">
+                    <span class="close">&times;</span>
+
+<div class="container">
+	<div class="jumbotron">
+		<form action="<?= URLROOT ?>/warehouseadmins/store" method="post">
+		  <div class="form-group">
+		    <label>Name</label>
+		    <input type="text" class="form-control" name="name" placeholder="Enter name">
+		  </div>
+		  <div class="form-group">
+		    <label>Total</label>
+		    <input type="text" class="form-control" name="total" placeholder="Enter total">
+		  </div>
+          <div class="form-group">
+		    <label>Outstanding</label>
+		    <input type="text" class="form-control" name="outstanding" placeholder="Enter outstanding">
+		  </div>
+          <div class="form-group">
+		    <label>Available</label>
+		    <input type="text" class="form-control" name="available" placeholder="Enter available">
+		  </div>
+        <br>
+		  <button type="submit" class="btn btn-primary">Submit</button>
+		</form>
+	</div>
+</div>
+</div>
+</div>
 <!-- // Require end include -->
 <?php require_once APPROOT . '\views\includes\end.php'; ?>
+
